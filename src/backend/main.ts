885cee6cfa06be4812ff62eb9e25bb95d8d266ca
Matthew Valentine <matthew.aaron.valentine+git@gmail.com>
2020-05-16 13:01:14 -0700

    Oauth is not quite working yet, but getting there.

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,11 +1,56 @@
 import * as express from 'express';
 import * as path from 'path';
+import {OauthProvider} from "./oauth";
+import session from "express-session";
+
+const oauth = new OauthProvider(
+    './secret/oauth_secret.json',
+    'http://localhost:8080/auth/google/callback',
+);
 
 const app = express();
+app.use(session({
+    secret: 'adults can eat Trix too',
+    cookie: {httpOnly: true},
+    // TODO: There are warnings about some parameters for which being optional has been deprecated.
+}));
 
 // Simple endpoint that returns the current time
-app.get('/api/time', function(req, res) {
+app.get('/api/time', (req, res) => {
     res.send(new Date().toISOString());
+});
+
+app.get('/auth/google/callback', async (req, res, next) => {
+    try {
+        const authCode = req.query.code as string;
+        const response = await oauth.newClient().getToken(authCode);
+
+        // TODO: Store in database instead of in session
+        const session = req.session!;
+        session.credentials = response.tokens;
+        // TODO: Is this necessary? Seems not to set the cookie if I don't.
+        session.save((err) => {
+            if (err) console.error(err);
+            res.redirect('/');
+        });
+    } catch(err) {
+        next(err);
+    }
+});
+
+// Redirect to sign in if necessary
+app.get('/', (req, res, next) => {
+    if (req.session?.credentials) {
+        // Already signed in.
+        next(null);
+        return;
+    }
+
+    const authorizeUrl = oauth.newClient().generateAuthUrl({
+        access_type: 'offline',
+        scope: ['profile', 'https://www.googleapis.com/auth/gmail.readonly'],
+    });
+    res.redirect(authorizeUrl);
 });
 
 // Serve static files
---
+++ src/backend/oauth.ts
@@ -0,0 +1,37 @@
+import {readFileSync} from 'fs';
+import {google} from 'googleapis';
+
+interface OauthSecret {
+    web: {
+        'client_id': string;
+        'client_secret': string;
+        'redirect_uris': string[];
+    }
+}
+
+export class OauthProvider {
+    private readonly secret: OauthSecret;
+    private readonly callbackUrl: string;
+
+    constructor(secretPath: string, callbackUrl: string) {
+        this.secret = JSON.parse(readFileSync(secretPath, 'utf8'));
+        this.callbackUrl = callbackUrl;
+        if (!this.secret.web.redirect_uris.includes(callbackUrl)) {
+            throw new Error(
+                `Callback url ${callbackUrl} is not registered (registered: ${this.secret.web.redirect_uris})`
+            );
+        }
+    }
+
+    newClient(credentials?: any /* TODO: Figure out how to import this type */) {
+        const client = new google.auth.OAuth2(
+            this.secret.web.client_id,
+            this.secret.web.client_secret,
+            this.callbackUrl,
+        );
+        if (credentials) {
+            client.setCredentials(credentials);
+        }
+        return client;
+    }
+}
